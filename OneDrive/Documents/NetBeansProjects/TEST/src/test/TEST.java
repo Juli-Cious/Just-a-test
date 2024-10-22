7a7221464a539211cb882936851b46b6d6a233be
Julius <winglessdragons123@gmail.com>
2024-10-22 11:33:29 +0800

    klj

--- OneDrive/Documents/NetBeansProjects/TEST/src/test/TEST.java
+++ OneDrive/Documents/NetBeansProjects/TEST/src/test/TEST.java
@@ -15,6 +15,8 @@ public class TEST {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Another World");
+        
     }
     
 }
