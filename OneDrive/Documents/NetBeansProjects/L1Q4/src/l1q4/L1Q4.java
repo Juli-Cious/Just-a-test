a7af993b696501fa23c997f863cd35178868174c
Julius <winglessdragons123@gmail.com>
2024-10-22 11:56:17 +0800

    More graphs

---
+++ OneDrive/Documents/NetBeansProjects/L1Q4/src/l1q4/L1Q4.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q4;
+
+/**
+ *
+ * @author justb
+ */
+public class L1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          System.out.println("Title: 2016 Monthly report"
+                        +  "\n3600|                                             "
+                        +  "\n3500|                                             "
+                        +  "\n3400|                                             "
+                        +  "\n3300|                                             "
+                        +  "\n3200|                                         *     "
+                        +  "\n3100|                                         *    "
+                        +  "\n3000|                                         *    "
+                        +  "\n2900|                                         *    "
+                        +  "\n2800|                                         *    "
+                        +  "\n2700|                               *         *     "
+                        +  "\n2600|                               *         *         "
+                        +  "\n2500|    *                          *         *      "
+                        +  "\n2400|    *                          *         *     "
+                        +  "\n2300|    *                          *         *      "
+                        +  "\n2200|    *                          *         *      "
+                        +  "\n2100|    *                          *         *      "
+                        +  "\n2000|    *                *         *         *       "
+                        +  "\n1900|    *                *         *         *       "
+                        +  "\n1800|    *                *         *         *       "
+                        +  "\n1700|    *                *         *         *       "
+                        +  "\n1600|    *       *        *         *         *        "
+                        +  "\n1500|    *       *        *         *         *        "
+                        +  "\n1400|    *       *        *         *         *        "
+                        +  "\n1300|    *       *        *         *         *        "
+                        +  "\n1200|    *       *        *         *         *        "
+                        +  "\n1100|    *       *        *         *         *            "
+                        +  "\n1000|    *       *        *         *         *            "
+                        +  "\n 900|    *       *        *         *         *            "
+                        +  "\n 800|    *       *        *         *         *       *     "
+                        +  "\n 700|    *       *        *         *         *       *  "
+                        +  "\n 600|    *       *        *         *         *       *  "
+                        +  "\n 500|    *       *        *         *         *       *   "
+                        +  "\n 400|    *       *        *         *         *       * "   
+                        +  "\n 300|    *       *        *         *         *       * "
+                        +  "\n 200|    *       *        *         *         *       * "
+                        +  "\n 100|    *       *        *         *         *       *     "
+                        +  "\n    ________________________________________________________"
+                        +  "\n        Jan     Feb     March     April      May     Jun                                       ");
+                                         
+    }
+    
+}
